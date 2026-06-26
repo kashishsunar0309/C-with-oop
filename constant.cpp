692e64365e0aa629d0d7726f5e39210b7ed5b2d7
kashish0903 <tomshreepali@gmail.com>
2026-06-26 10:06:52 +0545

    add multiple and constant concept

---
+++ constant.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	const float pk = 3.14;
+	int k = 18;
+	cout << pk << " and " << k;
+	return 0;
+}
---
+++ two_value.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b, num;
+	cout << "Enter the number a: ";
+	cin >> a;
+	cout << "Enter the number b: ";
+	cin >> b;
+	num = a*b;
+	cout << a << " and " << b  << " is the multiple " << num << endl;
+	return 0;
+}
