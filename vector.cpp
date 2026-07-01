fe5d7e6cae101a5f4f21bca42a0e72a6dae16c33
kashish0903 <tomshreepali@gmail.com>
2026-07-01 11:36:35 +0545

    add vector function

---
+++ vector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v = {10, 20, 30};
+    cout << v.size();
+    cout << v.capacity();
+    v.push_back(40);
+    cout << v.size();
+    cout << v.capacity();
+    return 0;
+}
