44b6ed477abf5bdba47f9748ec0409fa5fd7c9f4
kashish0903 <tomshreepali@gmail.com>
2026-07-18 17:25:01 +0545

    add increament/decrement

---
+++ question.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e, f;
+    a = 10;
+    b = 12;
+    c = a + 1;
+    d = b + 1;
+    e = a - 1;
+    f = b - 1;
+    cout << "The incremented value of a = " << c << endl;
+    cout << "The incremented value of b = " << d << endl;
+    cout << "The decremented value of a = " << e << endl;
+    cout << "The decremented value of b = " << f << endl;
+    return 0;
+}
