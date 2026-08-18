e9066b6d0117976c2c07b4c81004206c710193f2
kashish0903 <tomshreepali@gmail.com>
2026-08-18 13:48:39 +0545

    add Desconstructor

---
+++ Desconstructor.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class Employee
+{
+public:
+    Employee()
+    {
+        cout << "Constructor" << endl;
+    }
+    ~Employee()
+    {
+        cout << "Desconstructor" << endl;
+    }
+};
+int main()
+{
+    Employee e1;
+    Employee e2;
+    return 0;
+}
