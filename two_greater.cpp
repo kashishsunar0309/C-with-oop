b699c5a45d1aa6cfd6c98a198f84bd6a799f9deb
kashish0903 <tomshreepali@gmail.com>
2026-08-29 13:02:09 +0545

    add two_greater finder

---
+++ two_greater.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    a = 2;
+    b = 3;
+    if (a > b)
+    {
+        cout << "a is greater than b";
+    }
+    else
+    {
+        cout << "b is greater than a";
+    }
+    return 0;
+}
