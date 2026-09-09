2a7fa734ef37c81b917a55e445c0ae9fb82385f9
kashish0903 <tomshreepali@gmail.com>
2026-09-09 12:54:21 +0545

    add array

---
+++ array_sample.cpp
@@ -0,0 +1,24 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    string name[] = {"bimarsha","kripesh","manan","nirmal"};
+    cout << name[1] << endl;
+    cout << name[0]<< endl;
+    cout << name[2]<< endl;
+    cout << name[3]<< endl;
+    return 0;
+}*/
+
+// INTEGER.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int num[] = {10,20,30,40,50};
+    cout << num[1] << endl;
+    cout << num[0]<< endl;
+    cout << num[2]<< endl;
+    cout << num[3]<< endl;
+    return 0;
+}
+*/
