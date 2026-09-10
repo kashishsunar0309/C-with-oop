1efc0ab2678716c1fdfebc9332daeb1b988e29b7
kashish0903 <tomshreepali@gmail.com>
2026-09-10 14:38:31 +0545

    add array_loop

---
+++ array_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string car[5] = {"BMW", "FORD", "PORSHE", "FERARI", "VOLVO"};
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << car[i] << ",";
+    }
+    return 0;
+}
